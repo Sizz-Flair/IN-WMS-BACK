45969bd3e68d3146afd23abd552b9cb4f79343f3
sizz <akfur222@naver.com>
2023-07-06 13:28:24 +0900

    Add StorageService
    재고관리 프로세스 생성

--- src/main/java/com/wms/inwms/controller/InboundController.java
+++ src/main/java/com/wms/inwms/controller/InboundController.java
@@ -3,6 +3,7 @@ package com.wms.inwms.controller;
 import com.wms.inwms.domain.inbound.InboundService;
 import com.wms.inwms.domain.inbound.dto.InboundResultDto;
 import com.wms.inwms.domain.inbound.dto.InboundSaveDto;
+import com.wms.inwms.domain.location.upperlocation.dto.UpperLocationDto;
 import com.wms.inwms.domain.response.ResponseData;
 import com.wms.inwms.domain.response.ResponseMessage;
 import com.wms.inwms.domain.response.ResultDataList;
@@ -81,36 +82,5 @@ public class InboundController {
     private ResponseEntity<ResultDataList> selectMapping(@RequestParam String mappingNum) {
         List<InboundResultDto.InboundSelectResultDto> resultData = inboundService.getSelectInboundData(mappingNum);
         return ResponseEntity.ok().body(responseData.ResultListData(resultData, ResponseMessage.SUCCESS.name()));
-    }
-
-    /**
-     * ==============================================
-     * <p> 재고내역 조회(상위 로케이션)
-     * ==============================================
-     * user : akfur
-     * date : 2023-06-28
-     *
-     * @return ResponseEntity<ResultDataList>
-     */
-    @GetMapping(path = "/serach/storage")
-    private ResponseEntity<ResultDataList> searchStorage() {
-        List<UpperLocationDto.UpperLocationResultDto> data = inboundService.getStorage();
-        return ResponseEntity.ok().body(responseData.ResultListData(data, ResponseMessage.SUCCESS.name()));
-    }
-
-    /**
-     * ==============================================
-     * <p> 재고내역 상세 조회
-     * ==============================================
-     * user : akfur
-     * date : 2023-06-28
-     *
-     * @param locationName
-     * @return ResponseEntity<ResultDataList>
-     */
-    @GetMapping(path = "/search/storage/detail")
-    private ResponseEntity<ResultDataList> searchDetailStr(@RequestParam String locationName) {
-        List<InboundResultDto.InboundSaveResultDto> data = inboundService.getDetailStorage(locationName);
-        return ResponseEntity.ok().body(responseData.ResultListData(data, ResponseMessage.SUCCESS.name()));
     }
 }
--- src/main/java/com/wms/inwms/domain/location/upperlocation/UpperLocationService.java
+++ src/main/java/com/wms/inwms/domain/location/upperlocation/UpperLocationService.java
@@ -1,9 +1,13 @@
 package com.wms.inwms.domain.location.upperlocation;
 
 import com.wms.inwms.domain.base.BaseService;
+import com.wms.inwms.domain.inbound.InboundEntity;
+import com.wms.inwms.domain.location.upperlocation.dto.UpperLocationDto;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class UpperLocationService extends BaseService<UpperLocation, Long> {
@@ -14,11 +18,21 @@ public class UpperLocationService extends BaseService<UpperLocation, Long> {
         this.repository = repository;
     }
 
-    public Optional<UpperLocation> findById(Long id) {
-        return repository.findById(id);
-    }
-
     public Optional<UpperLocation> findByName(String name) {
         return repository.findByUpLocationName(name);
+    }
+
+    /**
+     * ==============================================
+     * <p> 재고내역 모두 조회
+     * ==============================================
+     * user : akfur
+     * date : 2023-07-06
+     *
+     * @return List<UpperLocationDto.UpperLocationResultDto>
+     */
+    public List<UpperLocationDto.UpperLocationResultDto> getStorage() {
+        List<UpperLocation> data = this.select().from(qUpperLocation).fetch();
+        return data.stream().map(e -> e.convertUpperDto()).collect(Collectors.toList());
     }
 }
